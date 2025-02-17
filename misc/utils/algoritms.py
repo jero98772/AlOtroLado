6d962b278a66eded33cf1ff68f7745074c6a7ee9
jero98772 <jero98772@gmail.com>
2025-02-16 23:00:26 -0500

    creating a ghpages

--- misc/utils/algoritms.py
+++
@@ -1,9 +0,0 @@
-def readtxt(name):
-  """
-  readtxt(name) , return txt content as array ,element by line 
-  """
-  content = []
-  with open(name, 'r') as file:
-    for i in file.readlines():
-      content.append(str(i).replace("\n",""))
-  return content 
